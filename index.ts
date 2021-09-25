3271d41414135649d8b59748de7c2414ca526c32
Shareef Omar Abdelal <sabd0011@student.monash.edu>
2021-09-25 12:01:18 +1000

    Removed redundant code and added comments

--- index.ts
+++ index.ts
@@ -1,8 +1,11 @@
+// Import the relevant discord.js modules
 const { Client, Intents } = require('discord.js');
 const dotenv = require('dotenv');
 
+// Get environment variables from dotenv config
 dotenv.config();
 
+// Create a discord client instance
 const client = new Client({
 	intents: [
 		Intents.FLAGS.GUILDS,
@@ -10,11 +13,11 @@ const client = new Client({
 	],
 });
 
+// Prints to console.log when the 'ready' event is completed
+// 'ready' is when bot turns on
 client.on('ready', () => {
 	console.log('Rosen is awake');
-
-	// const guildId = 890918967684780053;
-	// const guild = client.guilds.cache.get(guildId);
 });
 
+// Use client token to login to discord
 client.login(process.env.TOKEN);
