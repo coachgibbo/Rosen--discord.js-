ebdbfc8de6af6075e5888f19a42f90e3024bf3f0
Shareef Omar Abdelal <sabd0011@student.monash.edu>
2021-09-26 19:09:05 +1000

    Added the /join command

--- commands/join.ts
+++ commands/join.ts
@@ -1,14 +1,41 @@
 /**
  * A command that tells the bot to join the user's channel
  */
-import { CommandInteraction } from "discord.js";
+import { CommandInteraction, GuildMember } from "discord.js";
 import { SlashCommandBuilder } from '@discordjs/builders';
+import { DiscordGatewayAdapterCreator, entersState, joinVoiceChannel, VoiceConnectionStatus } from "@discordjs/voice";
 
 module.exports = {
 	data: new SlashCommandBuilder() // Discord Command Builder
 		.setName('join') // Command Name
 		.setDescription('Joins the user\'s voice channel'), // Command Desc (Shown in disc)
 	async execute(interaction: CommandInteraction) {
-		await interaction.reply('Joining'); // Reply to the interaction (message)
+		var userChannel = (interaction.member as GuildMember).voice.channel; // Get caller's voice channel
+
+		// Handler for when caller is not in a voice channel
+		if (!userChannel) {
+			await interaction.reply(`You're not in a voice channel`);
+			return;
+		} else {
+			await interaction.reply(`Joining ${userChannel!.name}`);
+		}
+
+		// Create the VoiceConnection and set channel parameters from caller
+		const connection = joinVoiceChannel({
+			channelId: userChannel?.id as string,
+			guildId: userChannel?.guildId as string,
+			adapterCreator: userChannel?.guild.voiceAdapterCreator as DiscordGatewayAdapterCreator,
+		});
+
+		// Catch if the bot doesn't connect and destroy the connection
+		// Return it if connection is successful
+		try{
+			await entersState(connection, VoiceConnectionStatus.Ready, 15e3);
+			await interaction.editReply(`Joined ${userChannel!.name}`);
+			return connection;
+		} catch (error) {
+			connection.destroy();
+			throw error;
+		}
 	}
 }
