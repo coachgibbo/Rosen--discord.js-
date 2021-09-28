51bcf2afe9a10d40494756767a7a2e9293da3a12
Shareef Omar Abdelal <sabd0011@student.monash.edu>
2021-09-28 12:31:09 +1000

    Added basic (non-queue) functionality to /play

--- commands/play.ts
+++ commands/play.ts
@@ -7,10 +7,13 @@
  * 		5. Joins the callers voice channel if not already there
  * 		6. Plays the song
  */
-
 import { SlashCommandBuilder } from "@discordjs/builders";
 import { CommandInteraction } from "discord.js";
-const YoutubeSearch = require("../api/YoutubeSearch");
+import { AudioPlayerStatus, createAudioPlayer, getVoiceConnection } from "@discordjs/voice";
+import { Song } from "../resources/Song";
+
+const YoutubeSearch = require("../utilities/YoutubeSearch");
+const JoinUtility = require("../utilities/joinChannel");
 
 module.exports = {
 	data: new SlashCommandBuilder() // Discord Command Builder
@@ -25,9 +28,33 @@ module.exports = {
 		// Use Youtube API to grab search results. Indicate success to caller.
 		// await works for now. consider switching to promises if doing spotify stuff too
 		const searchResults = await YoutubeSearch.loadYoutube(searchQuery).catch(console.error);
-		await interaction.editReply(`Song retrieved with id: ${searchResults.data.items[0].id.videoId} :green_circle:`);
+		const videoId = searchResults.data.items[0].id.videoId
+		await interaction.editReply(`Song retrieved with id: ${videoId} :green_circle:`);
+
+		// Create a Song object with information necessary for playing the track
+		const resource = new Song({
+			id: videoId,
+			title: searchResults.data.items[0].snippet.title,
+			onStart: () => { },
+			onFinish: () => { },
+			onError: () => { }
+		}).createAudioResource();
+
+		// Create a discord audio player and load the audio resource
+		// Maybe move this into join and leave commands
+		const player = createAudioPlayer();
+		player.play(resource)
+		
+		// Check if voice connection exists and joins if not. Connect player to connection
+		let connection = getVoiceConnection(interaction.guildId!)
+		if (!connection) {
+			connection = await JoinUtility.joinChannel(interaction);
+		}
+		connection?.subscribe(player)
 
 		// Once this works completely, delete reply and use an embed for now playing
 		await interaction.editReply(`Now Playing: ${searchResults.data.items[0].snippet.title}`)
+
+		player.on(AudioPlayerStatus.Idle, () => console.log("Song Finished"));
 	}
 }
